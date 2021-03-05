07920601abc27c22dc0b0269b6a76b4785640f29
OlivierFrancois <olivier.francois09@gmail.com>
2021-03-05 09:50:46 +0100

    Responsiver header

--- public/js/index.js
+++ public/js/index.js
@@ -8,13 +8,24 @@
 /***/ (() => {
 
 var nav = document.querySelector('nav');
+var navItems = document.querySelectorAll('.nav-item');
 var btnMenu = document.querySelector('.toggle-nav');
 var lines = document.querySelector('.cont-lines'); //Quand on clique sur le bouton, toggle des class pour faire disparaitre le nav
 
 btnMenu.addEventListener('click', function () {
   lines.classList.toggle('active');
   nav.classList.toggle('menu-visible');
-});
+}); //Si la largeur de l'écran est < à 1300 px
+
+if (window.matchMedia('(max-width: 1300px)')) {
+  // Au clic d'un item du nav, on désactive le nav
+  navItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+      lines.classList.toggle('active');
+      nav.classList.toggle('menu-visible');
+    });
+  });
+}
 
 /***/ }),
 
