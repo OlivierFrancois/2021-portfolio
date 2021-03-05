1792fb47b043b1ec897d1a06a96fab562e8beafc
OlivierFrancois <olivier.francois09@gmail.com>
2021-03-05 09:50:30 +0100

    Hide/Display toggle menu button

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 const nav = document.querySelector('nav');
+const navItems = document.querySelectorAll('.nav-item');
+
 const btnMenu = document.querySelector('.toggle-nav');
 const lines = document.querySelector('.cont-lines');
 
@@ -7,3 +9,14 @@ btnMenu.addEventListener('click', () => {
 	lines.classList.toggle('active');
 	nav.classList.toggle('menu-visible');
 })
+
+//Si la largeur de l'écran est < à 1300 px
+if (window.matchMedia('(max-width: 1300px)')) {
+	// Au clic d'un item du nav, on désactive le nav
+	navItems.forEach(item => {
+		item.addEventListener('click', () => {
+			lines.classList.toggle('active');
+			nav.classList.toggle('menu-visible');
+		})
+	})
+}
